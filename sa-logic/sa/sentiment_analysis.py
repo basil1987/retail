5ae6558506cfae25974597f26932cd8c6e94adcc
tom <tom@example.com>
2020-02-25 03:10:22 +0000

    sa/sentiment_analysis.py

--- sa-logic/sa/sentiment_analysis.py
+++ sa-logic/sa/sentiment_analysis.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route("/analyse/sentiment", methods=['POST'])
 def analyse_sentiment():
+
     sentence = request.get_json()['sentence']
     polarity = TextBlob(sentence).sentences[0].polarity
     return jsonify(
